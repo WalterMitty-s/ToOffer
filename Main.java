02774450dab93511176b0514582770e583023274
YiWuS <2375061390@qq.com>
2020-09-03 19:35:20 +0800

    SolutionJZ7

--- Main.java
+++ Main.java
@@ -24,27 +24,6 @@ class TreeNode {
 }
 
 
-
-class SolutionJZ7 {
-    public int Fibonacci(int n) {
-        if(n==0||n==1)
-            return n;
-        return Fibonacci(n-1)+Fibonacci(n-2);
-    }
-    //简洁实现
-    public int Fibonacci2(int n) {
-        if(n==0||n==1)
-            return n;
-        int sum=1,num=0;
-        for(int i=2;i<n;i++){
-            sum=sum+num;
-            num=sum-num;
-        }
-        return sum;
-    }
-
-}
-
 class SolutionJZ8 {
     public int JumpFloor(int target) {
         if(target==1||target==0)
